9184b9a4e252a5ba31a8e4d83aff551f1d4dfe2c
glory <thfqls7828@gmail.com>
2026-02-12 20:47:09 +0900

    feat: 홈 페이지 UI를 초기 구현하고 임시 피드 항목 및 댓글 시트 기능을 추가합니다.

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:healthy_bag/domain/entities/feed/feed_entity.dart';
+import 'package:healthy_bag/domain/entities/user_entity.dart';
 import 'package:healthy_bag/presentation/comment/comment_sheet.dart';
 import 'package:healthy_bag/presentation/home/widgets/feed_item.dart';
 import 'package:healthy_bag/presentation/widgets/healthy_bag_logo.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final UserEntity user;
+  const HomePage({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
     // 임시 테스트용 데이터 (TODO: 나중에 Entity로 바꾸기)
     final testFeed = FeedEntity(
-      id: 'jin123', 
-      authorId: 'user456', 
-      authorimageUrl: 'https://picsum.photos/200/300', 
-      imageUrl: 'https://picsum.photos/800/300', 
-      content: '운동 많이 된다. 자기 전에 생각날거야', 
-      likes: 1000, 
-      comments: 30, 
+      id: 'jin123',
+      authorId: 'user456',
+      authorimageUrl: 'https://picsum.photos/200/300',
+      imageUrl: 'https://picsum.photos/800/300',
+      content: '운동 많이 된다. 자기 전에 생각날거야',
+      likes: 1000,
+      comments: 30,
       updatedAt: DateTime.now(),
-      );
-
+    );
 
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -30,7 +31,7 @@ class HomePage extends StatelessWidget {
       ),
       body: Builder(
         builder: (innerContext) => FeedItemWidget(
-          feed: testFeed, 
+          feed: testFeed,
           onCommentTap: () => _showCommentSheet(innerContext),
         ),
       ),
