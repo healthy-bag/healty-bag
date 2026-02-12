504280d0144b27717caf253837c7eebb359ca6f1
glory <thfqls7828@gmail.com>
2026-02-12 17:22:04 +0900

    feat: 초기 앱 설정 및 GoRouter 기반 앱 구조, 시작하기 버튼 UI를 추가했습니다.

--- lib/presentation/nickname/widgets/start_button.dart
+++ lib/presentation/nickname/widgets/start_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:healthy_bag/presentation/nickname/nickname_view_model.dart';
 
 class StartButton extends StatelessWidget {
