5c6a9a76b479dd6f23fb875b871fa1b0ac9aae59
kiwikid1543-alt <kiwikid1543@gmail.com>
2026-02-20 19:29:34 +0900

    feat: 피드 개수 계산 로직을 실제 게시물 수 기반으로 변경하고, 피드 삭제 시 사용자 피드 개수를 동기화합니다.

--- lib/presentation/my/viewmodel/my_tap_viewmodel.dart
+++ lib/presentation/my/viewmodel/my_tap_viewmodel.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:healthy_bag/core/di/repository_di/feed_repository_di.dart';
+import 'package:healthy_bag/core/di/repository_di/user_repository_di.dart';
 import 'package:healthy_bag/domain/entities/feed_entity.dart';
+import 'package:healthy_bag/presentation/notifier/global_user_notifier.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'my_tap_viewmodel.g.dart';
@@ -22,6 +24,11 @@ class MyTapViewmodel extends _$MyTapViewmodel {
   // }
 
   Future<void> deleteFeed(String feedId) async {
+    final user = ref.read(globalUserViewModelProvider);
     await ref.read(feedRepositoryProvider).deleteFeed(feedId);
+    // 삭제 후 전체 개수를 다시 세어서 동기화
+    await ref.read(userRepositoryProvider).addfeedCount(user!.uid);
+    // ui 업데이트를 위해 유저 정보도 다시 불러오기
+    ref.read(globalUserViewModelProvider.notifier).setUserById(user.uid);
   }
 }
